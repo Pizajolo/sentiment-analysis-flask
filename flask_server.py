33976004934e8406261697718e5191e68262eb61
simonpiazolo <simonpiazolo@gmail.com>
2019-09-08 17:52:17 +0200

    first version with news api

--- flask_server.py
+++ flask_server.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request,jsonify
 import tweepy
+import requests
+import datetime
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -12,6 +14,7 @@ consumer_key = lines[0].rstrip()
 consumer_secret = lines[1].rstrip()
 access_token = lines[2].rstrip()
 access_token_secret = lines[3].rstrip()
+news_api_key = lines[4].rstrip()
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -29,19 +32,41 @@ def index():
 
 @app.route("/search",methods=["POST"])
 def search():
-    search_tweet = request.form.get("search_query")
-    t = []
-    tweets = api.search(search_tweet, tweet_mode='extended')
     analyzer = SentimentIntensityAnalyzer()
-    for tweet in tweets:
-        vs = analyzer.polarity_scores(tweet.full_text)
-        if vs['compound'] >= 0.05:
-            sentiment = 1
-        elif vs['compound'] <= -0.05:
-            sentiment = -1
-        else:
-            sentiment = 0
-        t.append([tweet.full_text, vs['pos'], vs['neu'], vs['neg'], sentiment])
+    t = []
+    search_tweet = request.form.get("search_query")
+    source = 'news'
+    if source == 'twitter':
+        tweets = api.search(search_tweet, tweet_mode='extended')
+        for tweet in tweets:
+            vs = analyzer.polarity_scores(tweet.full_text)
+            if vs['compound'] >= 0.05:
+                sentiment = 1
+            elif vs['compound'] <= -0.05:
+                sentiment = -1
+            else:
+                sentiment = 0
+            t.append([tweet.full_text, vs['pos'], vs['neu'], vs['neg'], sentiment])
+    elif source == 'news':
+        date_object = datetime.date.today()
+        url = ('https://newsapi.org/v2/everything?'
+               'q=' + search_tweet +
+               '&language=en'
+               '&from=' + str(date_object) +
+               '&sortBy=popularity'
+               '&apiKey=' + news_api_key)
+        response = requests.get(url).json()
+        if response['status'] == 'ok':
+            for article in response['articles']:
+                vs = analyzer.polarity_scores(article['description'])
+                if vs['compound'] >= 0.05:
+                    sentiment = 1
+                elif vs['compound'] <= -0.05:
+                    sentiment = -1
+                else:
+                    sentiment = 0
+                t.append([article['title'], vs['pos'], vs['neu'], vs['neg'], sentiment])
+        print(t)
     return jsonify({"success":True,"tweets":t})
 
 
